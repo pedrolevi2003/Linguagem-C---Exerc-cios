de0ef198ea188bf74657b335d7a70f65dcf966d3
Pedro Levi Freitas Nascimento <pedro.graduacaounirio@gmail.com>
2025-10-17 13:46:38 -0300

    Exercício 24 feito

--- capitulo1/exercicio24desafio.c
+++ capitulo1/exercicio24desafio.c
@@ -7,13 +7,42 @@ possível. Assuma que a quantidade fornecida pelo cliente é sempre múltipla de
 
 int main()
 {
-    float qtdDinheiro;
+    int qtdDinheiro, qtd10 = 0, qtd20 = 0, qtd50 = 0, qtd100 = 0;
 
     printf("Digite a quantidade de dinheiro a sacar: ");
-    scanf("%f", &qtdDinheiro);
+    scanf("%d", &qtdDinheiro);
 
-    if(qtdDinheiro > 100)
+    if(qtdDinheiro >= 100)
     {
-        
+        qtd100 = qtdDinheiro / 100;
+
+        qtdDinheiro = qtdDinheiro - qtd100 * 100;
     }
+
+    if(qtdDinheiro >= 50)
+    {
+        qtd50 = qtdDinheiro / 50;
+
+        qtdDinheiro = qtdDinheiro - qtd50 * 50;
+    }
+
+    if(qtdDinheiro >= 20)
+    {
+        qtd20 = qtdDinheiro / 20;
+
+        qtdDinheiro = qtdDinheiro - qtd20 * 20;
+    }
+
+    if(qtdDinheiro >= 10)
+    {
+        qtd10 = qtdDinheiro / 10;
+
+        qtdDinheiro = qtdDinheiro - qtd10 * 10;
+    }
+
+    printf("Notas de 10: %d\n", qtd10);
+    printf("Notas de 20: %d\n", qtd20);
+    printf("Notas de 50: %d\n", qtd50);
+    printf("Notas de 100: %d\n", qtd100);
+
 }
