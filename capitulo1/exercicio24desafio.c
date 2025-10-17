53c87c2bf6cb0d24ab772ae905f1d54c09cbb7ec
Pedro Levi Freitas Nascimento <pedro.graduacaounirio@gmail.com>
2025-10-16 23:34:30 -0300

    Desafio 24 incompleto

---
+++ capitulo1/exercicio24desafio.c
@@ -0,0 +1,19 @@
+/* Sabendo que um caixa eletrônico possui notas de R$ 10, R$ 20, R$ 50 e R$ 100, leia um valor que
+representa a quantidade de dinheiro que o cliente deseja sacar e imprima a quantidade necessária de
+cada uma dessas notas para formar o valor desejado. A quantidade total de notas deve ser a mínima
+possível. Assuma que a quantidade fornecida pelo cliente é sempre múltipla de 10. */
+
+#include <stdio.h>
+
+int main()
+{
+    float qtdDinheiro;
+
+    printf("Digite a quantidade de dinheiro a sacar: ");
+    scanf("%f", &qtdDinheiro);
+
+    if(qtdDinheiro > 100)
+    {
+        
+    }
+}
